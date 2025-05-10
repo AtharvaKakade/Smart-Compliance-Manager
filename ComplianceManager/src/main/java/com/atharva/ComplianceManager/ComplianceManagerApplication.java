bf5f3c16c0fe04de43356d919c71788d1a6c12a3
AtharvaKakade <kakadeatharva5@gmail.com>
2025-05-10 13:26:23 +0530

    a

---
+++ ComplianceManager/src/main/java/com/atharva/ComplianceManager/ComplianceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atharva.ComplianceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplianceManagerApplication.class, args);
+	}
+
+}
---
+++ ComplianceManager/src/test/java/com/atharva/ComplianceManager/ComplianceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atharva.ComplianceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplianceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
